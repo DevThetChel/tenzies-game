28aaecf7c5877e0714b7ce18a0da8321a3deaa19
DevThetChel <myathetchel572@gmail.com>
2025-02-07 17:26:49 +0700

    updated

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,37 +1,70 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+
 import "./App.css";
 import { Die } from "./components/Die";
+import { nanoid } from "nanoid";
 
 function App() {
   const [allDice, setAllDice] = useState(getAllNewDice());
+
+  const gameWon =
+    allDice.every((die) => die.isHeld) &&
+    allDice.every((die) => die.number === allDice[0].number);
+
+  if (
+    allDice.every((die) => die.isHeld) &&
+    allDice.every((die) => die.number === allDice[0].number)
+  ) {
+    alert("Game Won!");
+    // setGameWon(true);
+  }
+
   function getAllNewDice() {
-    // const numbersArray = [];
-    // for (let i = 0; i < 10; i++) {
-    //   const randomNumber = Math.ceil(Math.random() * 6);
-    //   numbersArray.push(randomNumber);
-    // }
-    // return numbersArray;
+    const numbersArray = [];
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.ceil(Math.random() * 6);
+      numbersArray.push({ number: randomNumber, isHeld: false, id: nanoid() });
+    }
+    return numbersArray;
 
     // Or
 
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    // return new Array(10)
+    //   .fill(0)
+    //   .map(() => ({ number: Math.ceil(Math.random() * 6), isHeld: false , id: nanoid()}));
   }
 
   function rollDice() {
-    setAllDice(getAllNewDice());
+    setAllDice((die) =>
+      die.map((die) =>
+        die.isHeld ? die : { ...die, number: Math.ceil(Math.random() * 6) }
+      )
+    );
   }
 
-  const diceElements = allDice.map((diceNumber, index) => (
-    <Die key={index} value={diceNumber} />
+  function holdNumber(id) {
+    console.log(id);
+
+    setAllDice((prev) =>
+      prev.map((die) => (die.id === id ? { ...die, isHeld: !die.isHeld } : die))
+    );
+  }
+
+  const diceElements = allDice.map((die) => (
+    <Die key={die.id} die={die} holdNumber={holdNumber} />
   ));
+
   return (
     <>
       <div className="main">
+        <h3 className="title">Tenzies</h3>
+        <p className="des">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
         <div className="dies-container">{diceElements}</div>
         <button onClick={rollDice} className="roll-button">
-          Roll
+          {gameWon ? "Next game" : "Roll"}
         </button>
       </div>
     </>
--- tenzies-game/src/components/Die.jsx
+++ tenzies-game/src/components/Die.jsx
@@ -1,8 +1,17 @@
 import "../css/die.css";
 export function Die(props) {
+  console.log(props.die.isHeld);
+
   return (
     <>
-      <button className="die-button">{props.value}</button>
+      <button
+        onClick={() => {
+          props.holdNumber(props.die.id);
+        }}
+        className={props.die.isHeld ? "die-button held" : "die-button"}
+      >
+        {props.die.number}
+      </button>
     </>
   );
 }
